d627e44b46847b6b5a64b9ccf81074139e17c8fb
Jesús Pereira <jesusbotw@gmail.com>
2020-02-22 14:44:52 -0400

    new templates

--- src/index.php
+++ src/index.php
@@ -2,8 +2,17 @@
 
 require '../vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '\views');
+$router = new \Bramus\Router\Router();
 
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '\views');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('index.twig', ['name' => 'app']);
+$router->get('/', function () use ($twig) {
+    echo $twig->render('index.twig', ['name' => 'app']);
+});
+
+$router->get('/add', function () use ($twig) {
+    echo $twig->render('add.twig');
+});
+
+$router->run();
